3083dba7fa1f01073dec40664dbe5b742c75b4cf
ManarAhmedFawzy <manar-ahmed96@hotmail.com>
2019-02-13 00:37:46 +0200

    search part in page1 is done

--- page1.js
+++ page1.js
@@ -1,4 +1,34 @@
-window.addEventListener('load',function(){
+$(function(){
+    var images=["download%20(2).jpg","download%20(3).jpg","download%20(4).jpg","images.jpg","download%20(1).jpg"];
+            console.log(images);
 
-    
+    $("#nextimg").click(function(){
+        console.log(images);
+       document.images[0].src=images[0];
+       document.images[1].src=images[1];
+       document.images[2].src=images[2];
+        var x=images.splice(0,1);
+       
+        images.push(x.toString());
+        
+    });
+  
+      var arr=[];
+            function ajax(){
+             return $.ajax({
+                  method:"GET",
+                  url:"data.json",
+                  statusCode:{200:function(e){
+                  for(i in e)
+                      { 
+                          arr.push(e[i].city);
+                      }
+                  }}
+              });
+                
+              
+                   };
+            $.when(ajax()).done(function(){  let City = [...new Set(arr)];$('#autocomplete').autocomplete(
+                {lookup:City})});
+                
 });
