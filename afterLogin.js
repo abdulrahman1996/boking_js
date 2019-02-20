d0853cb93bff0fbe29511520725ad126f14198df
ManarAhmedFawzy <manar-ahmed96@hotmail.com>
2019-02-20 17:54:52 +0200

    last

--- afterLogin.js
+++ afterLogin.js
@@ -18,3 +18,14 @@
  $('#wel').click(function(){
         window.open("profilepage.html","_self");
     });
+
+ $('#hid').click(function(){
+       if(localStorage.user)
+           {
+                localStorage.removeItem('user');
+       localStorage.removeItem('logged');
+       localStorage.removeItem('mail');
+        window.open('pag1.html',"_self");
+           }
+      
+    });
